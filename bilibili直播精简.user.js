a67464fc8b3707a128023c86cceedcf013023da3
BackRunenr <liaoyp126@126.com>
2017-11-08 22:38:51 +0800

    update bilibili 3.0

--- bilibili直播精简.user.js
+++ bilibili直播精简.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili直播精简
 // @namespace    https://coding.net/u/BackRunner/p/GreaseMonkey-JS/git
-// @version      2.2
+// @version      3.0 beta
 // @description  【可能是你遇到的最好用的Bilibili直播精简脚本】，送礼随心开关，页面清爽无广告！
 // @author       BackRunner
 // @match        *://live.bilibili.com/*
@@ -18,8 +18,9 @@
 // 建议临时开启部分功能不要更改下面的代码，请临时禁用这个脚本
 // 注释掉部分代码可能会影响整体美观
 // == 更新日志 ==
-// 2017.04.23 - 2.2
-// 更新聊天框的过滤规则
+// 2017.11.08 - 3.0
+// 简单地针对新版的Bilibili直播进行了重制，后续将更新更多的调整。
+// 如果遇到Bug，请及时反馈
 // ==============
 
 
@@ -78,93 +79,54 @@
 
 	function addCSS(){
 		//变量定义
-		var cssText = "";
+        var cssText = "";
 
-		//去广告Banner
-		cssText += '.live-ad-full {display:none !important;}';
-		//去顶部下载
-		cssText += '.download-link {display:none !important;}';		
-		//去除左边栏
-		cssText += '#room-left-sidebar {display:none !important;}';
-		//背景调整
-		cssText += '.bk-img {transform:translate(0,0) !important;-webkit-transform:translate(0,0) !important;}';
-		//主体位置调整
-		cssText += '.room-main-ctnr {transform:translate(0,0) !important; -webkit-transform:translate(0,0) !important;}';
+        //右侧3D
+        cssText += ".haruna-canvas {display:none !important}";
+        //右边栏
+        cssText += ".side-bar-cntr {display:none !important}";
+        //许愿
+        cssText += ".ema-wishing {display:none !important}";
+        //活动banner
+        cssText += ".activity-pushing-out {display:none !important}";
+        //收获值
+        //cssText += ".activity-top3 {display:none !important}";
+        //关注按钮冒出来的萝莉
+        cssText += ".loli-ctnr {display:none !important}";
+        //最小化图标
+        cssText += ".minimize-btn {display:none !important}";
 
-		if (hideGift){
-			//去除礼物面板
-			cssText += '.gift-panel.main-section {display:none !important;}';
-			//去除聊天框低于1000礼物提示
-			cssText += '.gift-msg-1000.p-absolute {display:none !important;}';
-			//去除礼物连击
-			cssText += '.super-gift-ctnr {display:none !important;}';
-			//移除播主信息中的礼物信息
-			cssText += '.activity-top-panel {display:none !important;}';
-			cssText += '.row-item {display:none !important;}';
-			//移除银瓜子宝箱
-			cssText += '.treasure-box-ctnr {display:none !important;}';
-			//礼物信息
-			cssText += '#chat-msg-list .gift-msg {display: none !important;} #chat-list-ctnr > .super-gift-ctnr, #gift-msg-1000 {display: none !important;} #chat-list-ctnr > #chat-msg-list {height: 100% !important;}';
-			//聊天框内的各种公告（系统公告、小电视抽奖）
-			cssText += '.announcement-container {display:none !important;}';
-		}
-		//保持播主信息显示
-		cssText += '.bili-link {display:inline-block !important;}';
+        //弹幕框系统信息
+        cssText += ".system-msg {display:none !important}";
+        //弹幕框礼物信息
+        cssText += ".gift-item {display:none !important}";
+        cssText += ".super-gift-bubbles {display:none !important}";
+        //右侧头像
+        cssText += ".avatar-btn {display:none !important}";
 
-		//移除我要直播
-		cssText += '.live-action-btn-ctnr {display:none !important;}';
-		//移除整个footer
-		cssText += '.live-footer.p-relative {display:none !important;}';	
+        //footer
+        cssText += "#link-footer-vm {display:none !important}";
 
-		//去聊天框内用户等级信息、VIP标志、等级信息等等
-		if (hideUserInfo){
-			//去除排行榜
-			cssText += '.rank-list-ctnr {display:none !important;}';
-			//用户vip标志
-			cssText += '#chat-msg-list a[href="/i#to-vip"] {display: none !important;}';
-			//奖牌标志
-			cssText += '#chat-msg-list .medal-icon {display: none !important;}';
-			//用户等级标志
-			cssText += '#chat-msg-list .user-level-icon {display: none !important;}';
-			//称号标志
-			cssText += '#chat-msg-list a[href="/i/myTitle#list"] {display: none !important;}';
-			//Guard
-			cssText += '.guard-icon-small {display:none !important}';
-			cssText += '.guard-msg {display:none !important};';			
-			//弹幕字数限制提示css调整
-			cssText += '.danmu-length-count {top:22px !important;}';
-			//聊天发送框css调整
-			cssText += '.danmu-textbox.float-left {height:65px !important;}';
-			cssText += '.danmu-send-btn {height:85px !important;}';
-			//聊天发送框下排3个按钮
-			cssText += '.profile-ctrl {display:none !important;}';
-			//聊天面板css调整
-			cssText += '#chat-ctrl-panel {padding:20px 10px 23px 10px !important;}';
-			//各种span类的用户标志
-			cssText += '.check-my-title {display:none !important;}';
-			//爷
-			cssText += '.vip-icon {display:none !important;}';
-		}
 
-		//聊天框内的系统公告（xxx进入了直播间）
-		cssText += '.system-msg {display:none !important;}';
+        if (hideUserInfo){
+            cssText += "#rank-list-vm {display:none !important}";
+            cssText += ".chat-history-panel {height:calc(100% - 128px - 10px) !important}";
+            cssText += ".fans-medal-item {display:none !important}";
+            cssText += ".user-level-icon  {display:none !important}";
+            cssText += ".title-label {display:none !important}";
+            cssText += ".guard-icon {display:none !important}";
+            cssText += ".vip-icon {display:none !important}";
+            cssText += ".fans-medal-item-ctnr {display:none !important}";
+            cssText += ".left-action {display:none !important}";
+        }
 
-		//canvas
-		cssText += '#haruna-canvas {display:none !important;}';
+        if (hideGift){
+            cssText += "#gift-control-vm {display:none !important}";
+        } else {
+            cssText += ".v-top {display:inline-block !important}";
+            cssText += ".user-panel {padding-top:5px !important;top:0% !important}";
+        }
 
-		//顶部css调整（如果错位请注释掉下面这个部分）
-		cssText += '.search-bar-ctnr {padding-right:80px; !important}';
-		cssText += '.user-avatar {padding-right:40px; !important}';
-		try{
-			$('#top-nav-msg-hinter').css('right','60px');
-		}catch(e){
-			console.error(e);
-		}
-
-		//控制开关对应的css调整
-		cssText += '.chiyo-22 {display:none !important;}';
-		cssText += '.attention-btn-ctrl {margin-top:10px !important;margin-bottom:10px !important;}';
-		cssText += '.backrunner-btn {margin-right:5px;}';
 
 		var modStyle = document.querySelector('#modCSS');
 		if (modStyle === null)
@@ -177,23 +139,23 @@
 	}
 
 	function addControlButton(){
-		var before = document.getElementsByClassName('live-status-switcher')[0];
+		var before = document.getElementsByClassName('attention-btn-ctnr')[0];
 		//按钮添加
 		if (document.querySelector('#backrunner-giftCtrl')===null){			
 			//按钮层
 			var ctrlDiv=document.createElement("div");
 			ctrlDiv.setAttribute("id","backrunner-giftCtrl");
 			if (hideUserInfo){
-				ctrlDiv.innerHTML+='<button class="live-btn default float-right" id="backrunner-userCtrl-btn">隐藏用户信息</button>';
+				ctrlDiv.innerHTML+='<button class="backrunner-ctrlbtn" id="backrunner-userCtrl-btn">显示用户信息</button>';
 			} else {
-				ctrlDiv.innerHTML+='<button class="live-btn default float-right" id="backrunner-userCtrl-btn">显示用户信息</button>';
+				ctrlDiv.innerHTML+='<button class="backrunner-ctrlbtn" id="backrunner-userCtrl-btn">隐藏用户信息</button>';
 			}
 			if(hideGift){
-				ctrlDiv.innerHTML+='<button class="live-btn default float-right backrunner-btn" id="backrunner-giftCtrl-btn">开启礼物</button>';
+				ctrlDiv.innerHTML+='<button class="backrunner-ctrlbtn" id="backrunner-giftCtrl-btn">开启礼物</button>';
 			}else{
-				ctrlDiv.innerHTML+='<button class="live-btn default float-right backrunner-btn" id="backrunner-giftCtrl-btn">关闭礼物</button>';
+				ctrlDiv.innerHTML+='<button class="backrunner-ctrlbtn" id="backrunner-giftCtrl-btn">关闭礼物</button>';
 			}
-			before.parentNode.appendChild(ctrlDiv);
+			before.appendChild(ctrlDiv);
 		}				
 		//添加事件监听
 		addControlButtonEventListener();
@@ -259,12 +221,12 @@
 			}
 			switch (version){
 				default:
-					s_update += "版本已从 " + version + " 更新为 " + GM_info.script.version + "\n\n" + GM_info.script.version + "版本的更新内容为：\n更新聊天框的过滤规则\n";
+					s_update += "版本已从 " + version + " 更新为 " + GM_info.script.version + "\n\n" + GM_info.script.version + "版本的更新内容为：\n简单地针对新版的Bilibili直播进行了重制，后续将更新更多的调整。\n如果遇到Bug请及时反馈。\n";
 					break;                    
 				case "未知":
 					s_update += "欢迎使用 Bilibili直播页面精简脚本 by BackRunner\n您当前的脚本版本为： " + version + "\n您可以通过页面上的按钮开关礼物面板和用户信息显示\n";
 					break;             
-				case "2.2":
+				case "3.0 beta":
 					s_update += "版本已从 " + version + " 降级为 " + GM_info.script.version + "\n\n" + "建议使用最新版本的脚本以获得最佳体验\n降级会造成您的设置丢失，请检查您的设置\n";   
 					break;
 			}
