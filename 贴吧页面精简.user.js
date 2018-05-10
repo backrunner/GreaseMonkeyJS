43afac8b8778f2cb605ffdb2316387fb82224a96
BackRunner <liaoyp126@126.com>
2018-05-10 23:05:50 +0800

    update 2.7.9

--- 贴吧页面精简.user.js
+++ 贴吧页面精简.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://greasyfork.org/zh-CN/scripts/23687
 // @namespace    https://coding.net/u/BackRunner/p/GreaseMonkey-JS/git
 // @contributionURL https://sinacloud.net/backrunner/img/alipay.jpg
-// @version      2.7.8
+// @version      2.7.9
 // @description  【可能是你遇到的最好用的贴吧精简脚本】，完全去除各种广告及扰眼模块，全面支持各种贴吧页面，免登录看帖，【倒序看帖】
 // @author       BackRunner
 // @include      *://tieba.baidu.com/*
@@ -28,6 +28,8 @@
 // 如果您觉得本脚本好用可以赞助我一点零花
 // donate@backrunner.top (支付宝)
 // === 更新日志 ===
+// 2018.05.10 - 2.7.9
+// 添加固定开关以绕过一些兼容问题
 // 2018.05.09 - 2.7.8
 // 添加自动收起指引功能，该功能默认开启。
 // 2018.05.04 - 2.7.7
@@ -55,6 +57,8 @@
     var displayLive;
     var displaySign;
     var autoCloseGuide;
+    //脚本固定开关
+    var isCtrlPanelOn = true;
     //底部信息div
     var foot;
     var homePageMatch = RegExp("(http|https):\/\/tieba.baidu.com\/(#*$)|(http|https):\/\/tieba.baidu.com\/(index\.html#*$)");
@@ -93,7 +97,9 @@
             //控制面板
             try{
                 console.warn('贴吧页面精简 by BackRunner: 正在创建控制面板');
-                createControlPanel();
+                if(isCtrlPanelOn){
+                   createControlPanel();
+                }
             } catch(e){
                 console.error(e);
             }
@@ -142,8 +148,9 @@
                 //控制面板
                 try{
                     console.warn('贴吧页面精简 by BackRunner: 正在创建控制面板');
-                    createControlPanel();
-
+                    if(isCtrlPanelOn){
+                        createControlPanel();
+                    }
                 } catch(e){
                     console.error(e);
                 }
@@ -190,7 +197,9 @@
                 //控制面板
                 try{
                     console.warn('贴吧页面精简 by BackRunner: 正在创建控制面板');
-                    createControlPanel();
+                    if(isCtrlPanelOn){
+                        createControlPanel();
+                    }
                 } catch(e){
                     console.error(e);
                 }
@@ -214,7 +223,9 @@
             //控制面板
             try{
                 console.warn('贴吧页面精简 by BackRunner: 正在创建控制面板');
-                createControlPanel();
+                if(isCtrlPanelOn){
+                    createControlPanel();
+                }
             } catch(e){
                 console.error(e);
             }
@@ -637,12 +648,12 @@
                 default:
                     //版本更新时删除废弃变量
                     deleteTrashValue();
-                    s_update += "版本已从 " + version + " 更新为 " + GM_info.script.version + "\n\n" + GM_info.script.version + "版本的更新内容为：\n添加自动收起指引功能，该功能默认开启。\n\n如果遇到Bug请及时提交反馈，感谢。\n\n【重要提醒！必看！】\n如果您没有安装Adblock，请安装Adblock以获得最佳体验\n\n由于这个脚本已经比较稳定，后续只修复Bug和根据贴吧的变化添补新功能\n";
+                    s_update += "版本已从 " + version + " 更新为 " + GM_info.script.version + "\n\n" + GM_info.script.version + "版本的更新内容为：\n添加固定开关以绕过一些兼容问题。\n\n如果遇到Bug请及时提交反馈，感谢。\n\n【重要提醒！必看！】\n如果您没有安装Adblock，请安装Adblock以获得最佳体验\n\n由于这个脚本已经比较稳定，后续只修复Bug和根据贴吧的变化添补新功能\n";
                     break;
                 case "未知":
                     s_update += "欢迎使用贴吧页面精简脚本 by BackRunner\n您当前的脚本版本为： " + version + "\n\n【关于设置】\n您可以通过右上角的设置面板设置相关功能以获得最佳体验\n\n【重要提醒！必看！】\n如果您没有安装Adblock，请安装Adblock以获得最佳体验\n\n由于这个脚本已经比较稳定，后续只修复Bug和根据贴吧的变化添补新功能\n";
                     break;
-                case "2.7.8":
+                case "2.7.9":
                     s_update += "版本已从 " + version + " 降级为 " + GM_info.script.version + "\n\n" + "建议使用最新版本的脚本以获得最佳体验\n降级会造成您的设置丢失，请检查您的设置\n";
                     break;
             }
