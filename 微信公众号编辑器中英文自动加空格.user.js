dbe647fb33b1b948a087f2bcd01c1eb74077a3c6
BackRunner <liaoyp126@126.com>
2018-07-25 14:49:50 +0800

    update mpweixin script

--- 微信公众号编辑器中英文自动加空格.user.js
+++ 微信公众号编辑器中英文自动加空格.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微信公众号编辑器中英文自动加空格
 // @namespace    https://coding.net/u/BackRunner/p/GreaseMonkey-JS/git
-// @version      1.3
+// @version      1.4
 // @description  在微信公众号编辑器中加入一个用于自动在中英文间添加空格的按钮
 // @author       BackRunner
 // @include      *mp.weixin.qq.com/cgi-bin/appmsg?t=media/appmsg_edit&action=edit*
@@ -53,10 +53,12 @@
 			var p4=/([\u4e00-\u9fa5]+)([0-9_])/gi;//汉字 数字
 			var p5 = /([A-Za-z_])([`~!@#$%^&*()_\-+=?:"{}|,.\/;'\\[\]·~！@#￥%&*——\-+={}‘’])([\u4e00-\u9fa5]+)/gi;//英文+符号 汉字
 			var p6 = /([\u4e00-\u9fa5]+)([`~!@#$%^&*()_\-+=?:"{}|,.\/;'\\[\]·~！@#￥%&*——\-+={}‘’])([A-Za-z_])/gi;//汉字 符号+英文
+            var p10 = /([0-9_])([`~!@#$%^&*()_\-+=?:"{}|,.\/;'\\[\]·~！@#￥%&*——\-+={}‘’])([\u4e00-\u9fa5]+)/gi;//数字+符号 汉字
+            var p11 = /([\u4e00-\u9fa5]+)([`~!@#$%^&*()_\-+=?:"{}|,.\/;'\\[\]·~！@#￥%&*——\-+={}‘’])([0-9_])/gi;//汉字 符号+数字
 			var p7 = /([\u4e00-\u9fa5]+)([<])([a])/gi;//汉字 括号
 			var p8 = /([a])([>])([\u4e00-\u9fa5]+)/gi;//括号 汉字
             var p9 = /([A-Za-z_])([0-9_])([\u4e00-\u9fa5]+)/gi;//英文+数字 汉字
-			s = s.replace(p9,'$1 $2 $3').replace(p1, '$1 $2').replace(p2, "$1 $2").replace(p3, "$1 $2").replace(p4, "$1 $2").replace(p5,"$1$2 $3").replace(p6,"$1 $2$3").replace(p7,"$1 $2$3").replace(p8,"$1$2 $3");
+			s = s.replace(p9,'$1 $2 $3').replace(p1, '$1 $2').replace(p2, "$1 $2").replace(p3, "$1 $2").replace(p4, "$1 $2").replace(p5,"$1$2 $3").replace(p6,"$1 $2$3").replace(p11,"$1 $2$3").replace(p10,"$1$2 $3").replace(p7,"$1 $2$3").replace(p8,"$1$2 $3");
 			plist[i].innerHTML = s;
 			//console.log(s);
 		}
