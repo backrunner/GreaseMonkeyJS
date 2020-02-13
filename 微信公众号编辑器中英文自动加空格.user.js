d359591c85b39d889c00bf502cd21d639ea6b482
BackRunner <liaoyp126@126.com>
2020-02-13 19:16:42 +0800

    update mpweixin script

--- 微信公众号编辑器中英文自动加空格.user.js
+++ 微信公众号编辑器中英文自动加空格.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微信公众号编辑器中英文自动加空格
 // @namespace    https://coding.net/u/BackRunner/p/GreaseMonkey-JS/git
-// @version      2020.2
+// @version      2020.3
 // @description  在微信公众号编辑器中加入一个用于自动在中英文间添加空格的按钮
 // @author       BackRunner
 // @include      *mp.weixin.qq.com/cgi-bin/appmsg?t=media/appmsg_edit*
@@ -20,7 +20,7 @@
 
     // const
 
-    const CJK = '\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff';
+    const CJK = '\u4e00-\u9fa5';
 
     const ANY_CJK = new RegExp(`[${CJK}]`);
 
