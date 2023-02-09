f710c388a34674778c150254c878ad8fdf107bfa
BackRunner <dev@backrunner.top>
2023-02-10 00:45:11 +0800

    fix: patch class ignore pattern

--- 强制使用字体 - 核心.user.js
+++ 强制使用字体 - 核心.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         强制使用字体 - 核心
 // @namespace    http://tampermonkey.net/
-// @version      1.8
+// @version      1.9
 // @description  所有 强制使用字体 脚本的核心脚本
 // @author       BackRunner
 // @include      *
@@ -10,13 +10,12 @@
 // ==/UserScript==
 function changeFont(s_font,mode){
 	if (s_font !==  ""){
-		
 		switch(mode){
 			case 0:
 				var element = document.createElement("link");
 				element.rel="stylesheet";
 				element.type="text/css";
-				element.href='data:text/css,*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
+				element.href='data:text/css,*:not([class*="icon"]):not([class*="video"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="player"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
 				document.documentElement.appendChild(element);
 				break;
 			case 1:
@@ -27,15 +26,15 @@ function changeFont(s_font,mode){
 						modStyle = document.createElement('style');
 						modStyle.id = 'modCSS_font';
 						document.body.appendChild(modStyle);
-					}   
-					modStyle.innerHTML = '*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
+					}
+					modStyle.innerHTML = '*:not([class*="icon"]):not([class*="video"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not([class*="player"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
 				},300);
 				break;
 			case 2:
 				var element = document.createElement("link");
 				element.rel="stylesheet";
 				element.type="text/css";
-				element.href='data:text/css,*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
+				element.href='data:text/css,*:not([class*="icon"]):not([class*="video"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not([class*="player"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
 				document.documentElement.appendChild(element);
 				setTimeout(function(){
 					var modStyle = document.querySelector('#modCSS_font');
@@ -44,15 +43,15 @@ function changeFont(s_font,mode){
 						modStyle = document.createElement('style');
 						modStyle.id = 'modCSS_font';
 						document.body.appendChild(modStyle);
-					}   
-					modStyle.innerHTML = '*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont !important;}';
+					}
+					modStyle.innerHTML = '*:not([class*="icon"]):not([class*="video"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]):not([class*="player"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont !important;}';
 				},300);
 				break;
 			case 3:
 				var element = document.createElement("link");
 				element.rel="stylesheet";
 				element.type="text/css";
-				element.href='data:text/css,*:not(i):not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]){font-family:' + s_font + ',Arial,"Material Icons Extended";}';
+				element.href='data:text/css,*:not(i):not([class*="icon"]):not([class*="video"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="player"]):not([class*="code"]){font-family:' + s_font + ',Arial,"Material Icons Extended";}';
 				document.documentElement.appendChild(element);
 				setTimeout(function(){
 					var modStyle = document.querySelector('#modCSS_font');
@@ -61,8 +60,8 @@ function changeFont(s_font,mode){
 						modStyle = document.createElement('style');
 						modStyle.id = 'modCSS_font';
 						document.body.appendChild(modStyle);
-					}   
-					modStyle.innerHTML = '*:not(i):not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]){font-family:' + s_font + ',"Material Icons Extended",Arial,"Material Icons Extended";}';
+					}
+					modStyle.innerHTML = '*:not(i):not([class*="icon"]):not([class*="video"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="player"]):not([class*="code"]){font-family:' + s_font + ',"Material Icons Extended",Arial,"Material Icons Extended";}';
 				},300);
 				break;
 		}
